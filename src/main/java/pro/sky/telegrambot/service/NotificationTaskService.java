73480c41db29dd4357081157c85f47f795d0812b
Tarasov Sergei <www.neon@mail.ru>
2023-02-01 20:22:37 +0500

    выполнил задания до 2.4, включительно

---
+++ src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
@@ -0,0 +1,19 @@
+package pro.sky.telegrambot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
+
+@Service
+public class NotificationTaskService {
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    public NotificationTaskService(NotificationTaskRepository notificationTaskRepository) {
+        this.notificationTaskRepository = notificationTaskRepository;
+    }
+
+    public void createNotificationTask(NotificationTask notificationTask) {
+        notificationTaskRepository.save(notificationTask);
+    }
+}
