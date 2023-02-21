ec9c92cb938810d98e2259a841e4589ee584df17
Tarasov Sergei <www.neon@mail.ru>
2023-02-21 17:57:50 +0500

    тестировал работу с кнопками в телеграм

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.*;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
@@ -17,6 +18,7 @@ import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -49,6 +51,9 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     }
 
     private void createNewTask(Update update) {
+        if (update.message() == null || update.message().text() == null) {
+            return;
+        }
         String inputMessage = update.message().text();
 
         Pattern pattern = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W+]+)");
@@ -67,13 +72,16 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         }
     }
 
-    private void replyToNewUser(Update update) {
-        if (update.message().text().equals("/start")) {
-            System.out.println(update.message().chat().id());
-            long chatId = update.message().chat().id();
-            String messageText = "Hello new User!";
-            SendMessage message = new SendMessage(chatId, messageText);
-            SendResponse response = telegramBot.execute(message);
+    private void replyToNewUser(Update update) throws NullPointerException {
+        if (null != update.message() && null != update.message().text()) {
+            if (update.message().text().equals("/start")) {
+                System.out.println(update.message().chat().id());
+                long chatId = update.message().chat().id();
+                String messageText = "Hello new User!";
+                SendMessage message = new SendMessage(chatId, messageText);
+                SendResponse response = telegramBot.execute(message);
+                setKeyboard();
+            }
         }
     }
 
@@ -91,8 +99,84 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             String messageText = task.getSendMessage();
             Long chatId = task.getChatId();
             SendMessage message = new SendMessage(chatId, messageText);
+            setButtons(message);
             SendResponse response = telegramBot.execute(message);
         }
+    }
+
+    public void setButtons(SendMessage sendMessage) {
+        // Создаем клавиуатуру
+        //ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        Keyboard keyboard = new ReplyKeyboardMarkup(
+                new KeyboardButton[]{
+                        new KeyboardButton("text"),
+                        new KeyboardButton("contact").requestContact(true),
+                        new KeyboardButton("location").requestLocation(true)
+                }
+        );
+
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton("Кнопка 1").url("www.google.com"),
+                        new InlineKeyboardButton("Кнопка 2").callbackData("callback_data"),
+                        new InlineKeyboardButton("Кнопка 3").switchInlineQuery("Привет матушка")
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton("Кнопка 4").callbackData("syaska"),
+                        new InlineKeyboardButton("Кнопка 5").callbackData("mosaska")
+                }
+                );
+
+        sendMessage.replyMarkup(inlineKeyboard);
+
+//        replyKeyboardMarkup.
+        //sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        /*replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);*/
+
+        // Создаем список строк клавиатуры
+        //List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        //KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        //keyboardFirstRow.add(new KeyboardButton(“Привет”));
+
+        // Вторая строчка клавиатуры
+        //KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        //keyboardSecondRow.add(new KeyboardButton(“Помощь”);
+
+        // Добавляем все строчки клавиатуры в список
+        //keyboard.add(keyboardFirstRow);
+        //keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        //replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    public void setKeyboard() {
+        Keyboard replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new String[]{"first row button1", "first row button2"},
+                new String[]{"second row button1", "second row button2"})
+                .oneTimeKeyboard(true)   // optional
+                .resizeKeyboard(true)    // optional
+                .selective(true);        // optional
+
+        Keyboard keyboard = new ReplyKeyboardMarkup(
+                new KeyboardButton[]{
+                        new KeyboardButton("text"),
+                        new KeyboardButton("contact").requestContact(true),
+                        new KeyboardButton("location").requestLocation(true)
+                }
+        );
+
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton("url").url("www.google.com"),
+                        new InlineKeyboardButton("callback_data").callbackData("callback_data"),
+                        new InlineKeyboardButton("Switch!").switchInlineQuery("switch_inline_query")
+                });
     }
 
 }
